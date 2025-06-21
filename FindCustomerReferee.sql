9b3cbf49fab67da06e29fa84de3ec322676a208c
MyElephantLover <jin.summer.1993@gmail.com>
2025-06-20 22:04:41 -0400

    added comments

--- FindCustomerReferee.sql
+++ FindCustomerReferee.sql
@@ -1,1 +1,5 @@
 select name from Customer where referee_id != 2 or referee_id is null
+
+-- referee_id means the customer who did the referral
+-- this can be ambiguous as we also called it referrer
+-- and the person being referred as referral
