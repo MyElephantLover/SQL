5cbb3ca4b33f8679b846baa96c31fc41702dede1
MyElephantLover <jin.summer.1993@gmail.com>
2025-07-17 20:48:25 -0400

    added

--- CalculateSpecialBonus.sql
+++ CalculateSpecialBonus.sql
@@ -4,3 +4,5 @@ from Employees
 order by employee_id
 ;
 
+-- substring(name, 1, 1) PostgreSQL string indexing is 1-based, so substring(name, 1, 1)
+-- gets the first character, not (0, 1), which was how I got it wrong in the first place 
