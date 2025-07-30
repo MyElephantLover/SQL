3b08a3fe7534425fac1e923de54e3e1bc27a6f03
MyElephantLover <jin.summer.1993@gmail.com>
2025-07-29 20:51:45 -0400

    edited

--- CombineTwoTables.sql
+++ CombineTwoTables.sql
@@ -1,3 +1,6 @@
+-- Write a solution to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+-- Return the result table in any order.
+
 select a.firstName, a.lastName, b.city, b.state 
 from Person a left join Address b 
 on a.PersonID = b.PersonID
