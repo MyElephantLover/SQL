7da4feb81379c954bfa65882e10420b34e6b9eca
MyElephantLover <jin.summer.1993@gmail.com>
2025-08-09 14:37:07 -0400

    commit

---
+++ Customer PlacingtheLargestNumberofOrders.sql
@@ -0,0 +1,26 @@
+-- Write a solution to find the customer_number for the customer who has placed the largest number of orders.
+-- The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+
+-- it seems to be I used sum(order_number) but it actually needs to be count(*)
+-- becasue it should not be the summation of the order numbers but the count of the orders
+
+-- my solution
+
+select customer_number
+from (
+    select count(*) as total_orders,
+    customer_id,
+    rank() over(order by count(*) desc) as rnk
+    from Orders group by customer_id
+) k where rnk = 1
+
+-- official solution
+
+select customer_number
+from Orders 
+group by 1
+order by count(*) desc 
+limit 1
+
+
+
