85a04496d7db0dd80e4cf95f431d0a8f33bc2707
MyElephantLover <jin.summer.1993@gmail.com>
2025-12-24 11:20:45 -0500

    commit

---
+++ squaring_an_argument.sql
@@ -0,0 +1,20 @@
+-- Now you have to write a function that takes an argument and returns the square of it.
+
+--# write your SQL statement here: 
+-- you are given a table 'square' with column 'n'
+-- return a table with:
+--   this column and your result in a column named 'res'
+
+create or replace function sqrt_func()
+returns table(n int, res bigint)
+language plpgsql 
+as $$
+begin
+return query -- in plpgsql, we need 'return query' to return rows
+select s.n,
+        s.n::bigint * s.n::bigint as res from square s; -- don't forget a semi-colon here
+end;
+$$;
+
+select * from sqrt_func();
+
