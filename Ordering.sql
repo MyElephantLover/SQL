aed224cd3df9d4007bf55f65972f9d0480fb5427
MyElephantLover <jin.summer.1993@gmail.com>
2025-12-26 14:01:17 -0500

    edited

--- Ordering.sql
+++ Ordering.sql
@@ -13,4 +13,6 @@ select id, ceo, motto, employees from companies
 order by employees desc, id asc;
 
 -- confused with this question, but looking twice, the table is companies, so the 'employees' column contains the total number of employees for each company
+-- during test cases, we need to add a tie breaker, id, in case there are the same # of employees
+
 
