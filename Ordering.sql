b4dec3dc30c822c81d1218d7b04a351b8f7649ad
MyElephantLover <jin.summer.1993@gmail.com>
2025-12-26 13:58:17 -0500

    commit

---
+++ Ordering.sql
@@ -0,0 +1,16 @@
+-- Your task is to sort the information in the provided table 'companies' by number of employees (high to low). Returned table should be in the same format as provided:
+
+-- companies table schema
+
+-- id
+-- ceo
+-- motto
+-- employees
+
+-- Solution should use pure SQL. Ruby is only used in test cases.
+
+select id, ceo, motto, employees from companies
+order by employees desc, id asc;
+
+-- confused with this question, but looking twice, the table is companies, so the 'employees' column contains the total number of employees for each company
+
